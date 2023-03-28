149e4e7121900ebc59c03e826af50478cd9babce
thirumurugan7 <thiru82003@gmail.com>
2023-03-28 22:19:19 +0530

    sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
+
+import { Navbar, Sidebar } from "./components";
 const App = () => {
   return (
     <div className="relative bg-[#13131a] sm:-8 p-4 flex flex-row min-h-screen  ">
-      <div className="sm:flex hidden mr-10 relative text-white">Sidebar</div>
+      <div className="sm:flex hidden mr-10 relative text-white">
+        <Sidebar />
+      </div>
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import { logo, sun } from "../assets";
+import { navlinks } from "../constants";
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;

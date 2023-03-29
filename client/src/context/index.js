0c455ac7df40a02fa910eaded9e4c65ea1c108e1
thirumurugan7 <thiru82003@gmail.com>
2023-03-29 16:10:46 +0530

    contract interaction

---
+++ client/src/context/index.js
@@ -0,0 +1,32 @@
+import React, { useContext, createContext } from "react";
+
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0xf03351491b09fA26C5262f43861b78B587aD5c78"
+  );
+
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    const data = await createCampaign([
+      address, //owner
+      form.title,
+    ]);
+  };
+};
